9b8b7549ade14bf3011686b330e4aad809d0146c
tsujita-yurika <kotoraneko11@gmail.com>
2023-12-25 18:41:46 +0900

    P89大きい方の数値を代入する命令

--- new/src/Hello.java
+++ new/src/Hello.java
@@ -3,8 +3,10 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		
-		System.out.println("猫" + "です");
+		int a = 5;
+		int b = 3;
+		int m = Math.max(a, b);	
+	System.out.println("比較実験：" +  a + "と" + b + "とで大きい方は" + m );
 		
 		  }
 			
