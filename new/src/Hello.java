c7c29a866e17f48bfb0c260e29e032276eed2aff
tsujita-yurika <kotoraneko11@gmail.com>
2023-12-27 16:29:41 +0900

    P90,91,92文字列を数値に変更、乱数発生、キーボードから入力を受け付ける命令

--- new/src/Hello.java
+++ new/src/Hello.java
@@ -2,11 +2,25 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		int a = 5;
-		int b = 3;
-		int m = Math.max(a, b);	
-	System.out.println("比較実験：" +  a + "と" + b + "とで大きい方は" + m );
+//		String age = "31";
+////		文字列を数値に変換する
+//		int n = Integer.parseInt(age); 
+//		System.out.println
+//			("あなたは来年、" + (n + 1) + "歳になりますね。");
+//		
+////		乱数を発生させる（上限つき）
+//		int r = new java.util.Random() .nextInt(90);
+//		System.out.println("あなたはたぶん" + r + "歳ですね？");
+//		
+//		キーボードから入力を受け付ける命令
+		System.out.println("あなたの名前を入力してください。");
+		String name = new java.util.Scanner(System.in) .nextLine();
+		System.out.println("あなたの年齢を入力してください。");
+		int age = new java.util.Scanner(System.in) .nextInt();
+		System.out.println
+			("ようこそ" + age + "歳の" + name + "さん");
+		
+		
 		
 		  }
 			
